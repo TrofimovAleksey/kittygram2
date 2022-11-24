3f7e8fd59cc6c39eebf94d2298bf169bf5e4c517
Трофимов Алексей <9939713@mail.ru>
2022-11-24 20:32:37 +0300

    add validate_birth_year and validate

--- cats/serializers.py
+++ cats/serializers.py
@@ -35,6 +35,18 @@ class CatSerializer(serializers.ModelSerializer):
                   'age')
         read_only_fields = ('owner',)
 
+    def validate(self, data):
+        if data['color'] == data['name']:
+            raise serializers.ValidationError(
+                'Имя не может совпадать с цветом!')
+        return data
+
+    def validate_birth_year(self, value):
+        year = dt.date.today().year
+        if not (year - 40 < value <= year):
+            raise serializers.ValidationError('Проверьте год рождения!')
+        return value
+
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
 
